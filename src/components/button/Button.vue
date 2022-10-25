715037e8c3882efde6f6e3b20ad1d26ef1172974
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-10-25 07:52:28 +0600

    Added general components - button, tabs

---
+++ src/components/button/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button>
+        <slot></slot> <!-- slot button content -->
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+    }
+</script>
+
+<style lang="scss" scoped>
+    button {
+        background: #3869ff;
+        color: #fff;
+        border: 0px solid;
+        padding: 10px 20px;
+        cursor: pointer;
+        transition: all;
+        border-radius: 4px;
+
+        &:hover{
+            opacity: 0.9;
+        }
+    }
+</style>
---
+++ src/components/tabs/Tabs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="wp-vue-kit-tabs">
+    <div class="tab-item">
+      <router-link to="/">Settings</router-link>
+    </div>
+    <div class="tab-item">
+      <router-link to="/list">List</router-link>
+    </div>
+    <div class="tab-item">
+      <router-link to="/graph">Graph</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./style.scss";
+
+export default {
+  name: "Tabs",
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
